f42a358720d8739dd73cd7b0a32c9400bdcc4266
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:03:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
+import java.util.Scanner;
+
+/*
+Çin zodyağı hesaplanırken kişinin doğum yılının 12 ile bölümünde kalana göre bulunur.
+Doğum Tarihi %12 = 0 ➜ Maymun
+Doğum Tarihi %12 = 1 ➜ Horoz
+Doğum Tarihi %12 = 2 ➜ Köpek
+Doğum Tarihi %12 = 3 ➜ Domuz
+Doğum Tarihi %12 = 4 ➜ Fare
+Doğum Tarihi %12 = 5 ➜ Öküz
+Doğum Tarihi %12 = 6 ➜ Kaplan
+Doğum Tarihi %12 = 7 ➜ Tavşan
+Doğum Tarihi %12 = 8 ➜ Ejderha
+Doğum Tarihi %12 = 9 ➜ Yılan
+Doğum Tarihi %12 = 10 ➜ At
+Doğum Tarihi %12 = 11 ➜ Koyun
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int yil, kalan;
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Lutfen dogum yilinizi giriniz: ");
+        yil = yazici.nextInt();
+
+        kalan = yil % 12;
+        if (kalan >= 0) {
+
+            if (kalan == 0)
+                System.out.println("Maymun");
+            else if (kalan == 1) {
+                System.out.println("Horoz");
+            } else if (kalan == 2) {
+                System.out.println("Kopek");
+            } else if (kalan == 3) {
+                System.out.println("Domuz");
+            } else if (kalan == 4) {
+                System.out.println("Fare");
+            } else if (kalan == 5) {
+                System.out.println("Okuz");
+            } else if (kalan == 6) {
+                System.out.println("Kaplan");
+            } else if (kalan == 7) {
+                System.out.println("Tavsan");
+            } else if (kalan == 8) {
+                System.out.println("Ejderha");
+            } else if (kalan == 9) {
+                System.out.println("Yilan");
+            } else if (kalan == 10) {
+                System.out.println("At");
+            } else if (kalan == 11) {
+                System.out.println("Koyun");
+            }
+
+
+        }
+        else
+            System.out.println("Hatali giris");
+
+
     }
 }
